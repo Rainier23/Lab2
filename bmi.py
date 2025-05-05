00ac95c69d08e001d1a85eab2cfc84c00f20fbd3
Rainier Ong <ongjhrainier30.24@ichat.sp.edu.sg>
2025-05-05 15:01:36 +0800

    updated in week 3

--- bmi.py
+++ bmi.py
@@ -15,10 +15,13 @@ def calculate_bmi(height, weight):
     # Conditional classification
     if bmi < 18.5:
         print("Category: Underweight")
+        return -1
     elif 18.5 <= bmi < 24.9:
         print("Category: Normal weight")
+        return 0
     elif bmi > 25:
         print("Category: Overweight")
+        return 1
     
 # Call the function
 calculate_bmi(weight=57, height=1.73)
