4a5aa419b97f83d1767ecf1341c1cbaf7b1e24c8
Rainier Ong <ongjhrainier30.24@ichat.sp.edu.sg>
2025-05-05 13:30:36 +0800

    Temperature with median

--- Lab2.py
+++ Lab2.py
@@ -17,12 +17,21 @@ def calc_min_max_temperature(temperature):
     maximum = max(temperature)
     print("Minimum temperature:" , minimum)
     print("maximum temperature:" , maximum)
-    
-    
+
+
+
+def calc_median_temperature(temperature):
+    import statistics
+    median = statistics.median(temperature)
+    print("Median: " , median)
+
+
+
 def main():
-    inputs=get_user_input()
+    inputs = get_user_input()
     calc_average_temperature(inputs)
     calc_min_max_temperature(inputs)
+    calc_median_temperature(inputs)
 
 
 if __name__ == "__main__":
