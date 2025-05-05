183bee1c55f0c5e04b0a8b6eae773de6f311425c
Rainier Ong <ongjhrainier30.24@ichat.sp.edu.sg>
2025-05-05 11:39:15 +0800

    corrected code

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,29 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas(e.g. 5, 67, 32)")
+
+def get_user_input():
+    temperature = input("Input temperatures: ")
+    Temp_list = temperature.split()
+    for i in range(len(Temp_list)):
+        Temp_list[i] = float(Temp_list[i])
+    return Temp_list
+
+def calc_average_temperature(temperature):
+    average = sum(temperature) / len(temperature)
+    print("Average temperture:" , average)
+
+def calc_min_max_temperature(temperature):
+    minimum = min(temperature)
+    maximum = max(temperature)
+    print("Minimum temperature:" , minimum)
+    print("maximum temperature:" , maximum)
+    
+    
+def main():
+    inputs=get_user_input()
+    calc_average_temperature(inputs)
+    calc_min_max_temperature(inputs)
+
+
+if __name__ == "__main__":
+    main()
